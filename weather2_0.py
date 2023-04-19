6fda7779e46fc0660a54ec1f079081392847f8f0
Евгений <65220665+MrIsrak@users.noreply.github.com>
2023-04-19 15:06:57 +0300

    Placed entry in the daily_weather

--- weather2_0.py
+++ weather2_0.py
@@ -20,12 +20,27 @@ daily_weather = customtkinter.CTkFrame(master=window, width=300, height=700)
 Wfont=customtkinter.CTkFont(family='Arial', size=24)
 Wfont=('Arial', 36)
 
+defoult = customtkinter.CTkFont(family='Calibri', size=14)
+defoult=('Calibri', 14)
+
 #create labels
 main_text = customtkinter.CTkLabel(master=window, text="Weather",font=Wfont)
+enter_your_city = customtkinter.CTkLabel(master=daily_weather, text="Enter your city",font=defoult)
+
+#create entry
+city_entry = customtkinter.CTkEntry(master=daily_weather, font=defoult)
+
+#place entry
+city_entry.place(in_=daily_weather,x=80,y=40)
 
 #place frames in window
 daily_weather.place(relx=0,rely=0)
+
+#place labels in windows
 main_text.place(x=579,y=0)
+enter_your_city.place(x=105,y=10)
+
+
 
 
 window.mainloop()
