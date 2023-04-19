c1c3fe1a235c510b91b8bf2ce9a1e18e799be944
Евгений <65220665+MrIsrak@users.noreply.github.com>
2023-04-19 14:33:41 +0300

    added weather 2.0

--- weather.py
+++ weather.py
@@ -2,12 +2,44 @@
 import python_weather
 import asyncio
 import os
+import tkinter as tk
 import ttkbootstrap as ttk
+from ttkthemes import ThemedTk
+from ttkbootstrap import *
 
-# create a window 
-window = ttk.Window(themename="lumen")
+
+# create a window and apply theme to window
+window = ThemedTk(theme="lumen")
 window.geometry("1200x700")
 
+#create a frame
+style = ttk.Style()
+style.configure('TFrame', background='#FF0000')
+daily_weather = ttk.Frame(window)
+
+
+# Labels for weather
+
+weather_label = ttk.Label(daily_weather, text="Weather")
+temp_label = ttk.Label(window, text="Temperature")
+wind_label = ttk.Label(window, text="Wind Speed")
+humidity_label = ttk.Label(window, text="Humidity")
+tip_label = ttk.Label(window, text="Tip", font=("Arial", 12, "italic"))
+
+
+
+
+
+#place frames
+daily_weather.grid(row=0, column=2)
+print(1)
+
+#place labels
+weather_label.grid(row=0, column=0)
+# temp_label.grid(row=1, column=0)
+# wind_label.grid(row=2, column=0)
+# humidity_label.grid(row=3, column=0)
+# tip_label.grid(row=4, column=0)
 
 
 #place window
---
+++ weather2_0.py
@@ -0,0 +1,29 @@
+import tkinter
+import customtkinter
+import python_weather
+import asyncio
+import os
+
+#theme settings
+customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+#create CTk window
+window = customtkinter.CTk()  # create CTk window like you do with the Tk window
+window.geometry("1200x700")
+
+#create frames
+daily_weather = customtkinter.CTkFrame(master=window, width=300, height=700)
+
+#create labels
+font=customtkinter.CTkFont(family='Arial', size=24)
+font=('Arial', 24)
+
+main_text = customtkinter.CTkLabel(master=window, text="Weather",)
+
+#place frames in window
+#daily_weather.place(relx=0,rely=0)
+main_text.place(x=579,y=0)
+
+
+window.mainloop()
