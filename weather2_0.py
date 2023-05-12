4ba8ae8970ca4023c616d304868f114bd7030937
Евгений <65220665+MrIsrak@users.noreply.github.com>
2023-05-12 19:12:46 +0300

    Tabview placed

--- weather2_0.py
+++ weather2_0.py
@@ -1,6 +1,7 @@
 #import modules
 import tkinter
 import customtkinter
+from customtkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from geopy.geocoders import Nominatim
@@ -11,6 +12,9 @@ from meteostat import Point, Daily
 
 #showing the weather information
 def get_weather(enter_your_city):
+
+    canvas.get_tk_widget().configure(background='white')
+
     #entering the city name
     user_text = enter_your_city.get()
     
@@ -35,8 +39,12 @@ def get_weather(enter_your_city):
     data = Daily(location_p, today, tomorrow)
     data = data.fetch()
 
+    
+
     # Plot line chart including average, minimum and maximum temperature
     fig = Figure(figsize=(3.5, 3.5), dpi=92)
+
+    
 
     x = y = range(1, 10)
     ax = fig.add_subplot(111)
@@ -51,11 +59,9 @@ def clear_graph():
     # Delete all elements on the canvas
     canvas.get_tk_widget().delete("all")
     # Update the canvas
-    canvas.get_tk_widget().configure(background='lightgray')
+    canvas.get_tk_widget().configure(background='#e0dcdc')
     canvas.draw()
     
-
-
 
 #theme settings
 customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
@@ -96,11 +102,11 @@ daily_highlight.grid(row=0, column=1, columnspan=3, rowspan=5, padx=10, pady=10,
 
 
 #create labels
-main_text = customtkinter.CTkLabel(master=window, text="Weather", font=Wfont)
-main_text.grid(row=0, column=0, columnspan=4, pady=20, sticky="n")
+#main_text = customtkinter.CTkLabel(master=daily_highlight, text="Weather", font=Wfont)
+#main_text.grid(row=0, column=0, columnspan=4, pady=20, sticky="n")
 
 enter_your_city = customtkinter.CTkLabel(master=daily_weather, text="Enter your city", font=defoult)
-enter_your_city.grid(row=0, column=0, padx=10, pady=10,)  
+enter_your_city.grid(row=0, column=0, padx=10, pady=10)  
 
 #create buttons
 show_weather = customtkinter.CTkButton(master=daily_weather, text="Show the weather", command=lambda:get_weather(city_entry))
@@ -108,9 +114,20 @@ show_weather.grid(row=2, column=0, pady=10)
 
 clear_graph = customtkinter.CTkButton(master=daily_weather, text="Clear Graph", command=clear_graph)
 clear_graph.grid(row=3, column=0, pady=10)  
+
 #create entry
 city_entry = customtkinter.CTkEntry(master=daily_weather, font=defoult)
 city_entry.grid(row=1, column=0, pady=10)  
+
+#create tabview
+tabview = customtkinter.CTkTabview(master=daily_highlight)
+tabview.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+
+
+tabview.add("tab 1")  # add tab at the end
+tabview.add("tab 2")  # add tab at the end
+tabview.set("tab 1")  # set currently visible tab
 
 #create canvas
 canvas = FigureCanvasTkAgg(Figure(figsize=(3.5, 3.5), dpi=92), master=daily_weather)
@@ -119,7 +136,10 @@ canvas.get_tk_widget().grid(row=4, column=0, padx=6, pady=4)
 
 #configure grid rows and columns
 daily_weather.columnconfigure(0, weight=1, minsize=200)
-daily_weather.rowconfigure(4, weight=1, minsize=300, pad=0)  # добавлен параметр pad=0
+daily_weather.rowconfigure(4, weight=1, minsize=300, pad=0)
+
+daily_highlight.columnconfigure(0, weight=1,minsize=830)
+daily_highlight.rowconfigure(0, weight=1,minsize=450)
 
 
 window.mainloop()
