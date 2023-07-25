b013c2b7cca46b5cadc0f842841897434b81732a
Евгений Уйстрайх <evgenii.uistraikh@gmail.com>
2023-07-25 23:46:58 +0300

    Added face detection

--- face rec/main.py
+++ face rec/main.py
@@ -1,21 +1,46 @@
 import face_recognition
 from PIL import Image, ImageDraw
+import os
 
+#Создание пути для открытия файла
+def img_path(img_name):
+    project_root = r"C:\Users\evgen\OneDrive\Рабочий стол\пайтон\AppHub\AppHub\face rec"
+    image_path = os.path.join(project_root, "imgs", img_name)
+    return image_path
 
+#Распознование и обвод лиц
 def face_rec():
-    mendale = face_recognition.load_image_file(r"C:\Users\evgen\OneDrive\Рабочий стол\пайтон\AppHub\AppHub\face rec\imgs\Mendale.jpg")
-    mendale_file_loc = face_recognition.face_locations(mendale)
+    #Ввод имени файла
+    img_name = input('img_name: ')
 
-    print(mendale_file_loc)
+    #Загрузка изображения и получение координат
+    photo = face_recognition.load_image_file(img_path(f'{img_name}.jpg'))
+    photo_file_loc = face_recognition.face_locations(photo)
 
-    men_pil = Image.fromarray(mendale)
-    draw_m = ImageDraw.Draw(men_pil)
+    #Отчет о количестве лиц
+    print(f"I`ve found {len(photo_file_loc)} face(s)")
+    
+    # Добавляем "_new" перед расширением файла
+    new_file_name = f"{img_name}_new.png"
 
-    for(top, right, bottom, left) in mendale_file_loc:
+    #Обведение лица
+    photo_pil = Image.fromarray(photo)
+    draw_m = ImageDraw.Draw(photo_pil)
+
+    for (top, right, bottom, left) in photo_file_loc:
         draw_m.rectangle(((left, top), (right, bottom)), outline=(255, 0, 255), width=4)
     del draw_m
 
-    men_pil.save(r"C:\Users\evgen\OneDrive\Рабочий стол\пайтон\AppHub\AppHub\face rec\imgs\Mendale_new.png")
+    #Сохранение в другю папку
+    project_root = r"C:\Users\evgen\OneDrive\Рабочий стол\пайтон\AppHub\AppHub\face rec"
+
+    new_file_path = os.path.join(project_root, "output", new_file_name)
+
+    photo_pil.save(new_file_path)
+ 
+    #Отчет о сохранении
+    print(f"saved by {new_file_path} path")
+
 
 def main():
     face_rec()
